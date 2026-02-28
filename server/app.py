14711efcde5a1185b435e21a4cc2c4cc2cec1c21
rick-kenti <erickmakenti@gmail.com>
2026-02-28 08:14:00 +0300

    update and delete routes with the associated actions to return the appropriate JSON data.

--- server/app.py
+++ server/app.py
@@ -21,7 +21,7 @@ class Plants(Resource):
 
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return plants, 200
 
     def post(self):
         data = request.get_json()
@@ -35,21 +35,44 @@ class Plants(Resource):
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return new_plant.to_dict(), 201
 
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
+class PlantById(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        return plant.to_dict(), 200
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+    def patch(self, id):
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        data = request.get_json()
+
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+
+        return plant.to_dict(), 200
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    def delete(self, id):
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204
+
+api.add_resource(PlantById, '/plants/<int:id>')
